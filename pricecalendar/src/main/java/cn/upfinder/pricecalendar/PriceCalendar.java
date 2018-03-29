c9480682ba2951d35deda77ea7dc940ce1ec00b0
ucm <824429779@qq.com>
2018-03-29 16:11:32 +0800

    Initial commit

---
+++ pricecalendar/src/main/java/cn/upfinder/pricecalendar/PriceCalendar.java
@@ -0,0 +1,26 @@
+package cn.upfinder.pricecalendar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by ucm on 2018/3/29.
+ */
+
+public class PriceCalendar extends RecyclerView {
+
+
+    public PriceCalendar(Context context) {
+        super(context);
+    }
+
+    public PriceCalendar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PriceCalendar(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
